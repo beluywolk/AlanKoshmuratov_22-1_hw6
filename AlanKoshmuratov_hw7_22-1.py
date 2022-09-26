0ff3f1785fd806872a49617e3e40546bf7c50707
beluywolk <alankoshmuratov42@gmail.com>
2022-09-26 23:04:12 +0600

    hw7

--- AlanKoshmuratov_hw7_22-1.py
+++ AlanKoshmuratov_hw7_22-1.py
@@ -1,15 +1,27 @@
 def binary_search(number, *number_list):
-    number_list = list(number_list)
-    a = min(number_list)
-    b = max(number_list)
-    c = 0
-    while c != number:
-        c = (a+b) // 2
-        if c < number:
-            a = c + 1
-        elif c > number:
-            b = c - 1
-        print(c)
+    N = 20
+    ResultOk = False
+    First = 0
+    Last = N - 1
+    while First < Last:
+        Middle = (number_list[First] + number_list[Last]) // 2
+        if number == number_list[Middle]:
+            First = Middle
+            Last = First
+            ResultOk = True
+            break
+
+
+        if number > number_list[Middle]:
+            First = Middle + 1
+        if number < number_list[Middle]:
+            Last = Middle - 1
+
+
+    if ResultOk:
+        print(f'элемент найден - {number_list[Middle]}')
+    else:
+        print('элемент не найден')
 def selection_sort(*args):
     args = list(args)
     args1 = []
@@ -26,15 +38,12 @@ def selection_sort(*args):
             break
 
     print(args)
-selection_sort(90, 4, 5, 8, 88, 2, 6)
+#selection_sort(90, 4, 5, 8, 88, 2, 6)
 
 
 
 
 
 
-binary_search(9, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,\
-              28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53\
-              , 54, 55, 56, 57, 58, 59, 60 )
-
+binary_search(6, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
 
